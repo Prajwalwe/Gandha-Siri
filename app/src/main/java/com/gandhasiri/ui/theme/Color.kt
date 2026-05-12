12e4b1cd676f9e736b979d458c6c268ebb1da2c1
Prajwal H P <prajwal.h.p11prajwal.h.p11@gmail.com>
2026-05-12 15:15:55 +0530

    Add custom color palette for UI theme

---
+++ app/src/main/java/com/gandhasiri/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.gandhasiri.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Custom Palette: Sandalwood & Natural Resources
+val SandalwoodTan = Color(0xFFE5D3B3)
+val DeepBark = Color(0xFF4B3621)
+val LeafGreen = Color(0xFF386641)
+val SecurityRed = Color(0xFFBC4749)
+
