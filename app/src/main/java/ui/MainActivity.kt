248ce2139b9805fade643fe199eb738a005c3ad2
Prajwal H P <prajwal.h.p11prajwal.h.p11@gmail.com>
2026-05-15 09:50:15 +0530

    Create MainActivity with UI setup and comments
    Added initial setup for MainActivity with UI logic comments.

---
+++ app/src/main/java/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.gandhasiri.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            // Gandha-Siri Minimalist Sandalwood UI logic
+            // Displays Tree Register and GPS location data
+        }
+    }
+}
